7b0bedef4bcf7cca046d01a6f6ec98f8fb971e20
R a b i a <rabia.saeed.1995@gmail.com>
2022-06-21 21:14:35 -0400

    updated seeds.sql and changed database

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,27 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    );
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
-    );
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+  ON DELETE CASCADE
+);
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
-    );
-
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  INDEX role_ind (role_id),
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) 
+  ON DELETE SET NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,34 +1,33 @@
-INSERT INTO department (id, name)
+INSERT INTO department (name)
     VALUES 
-    (1, 'IT'),
-    (2, 'HR'),
-    (3, 'Marketing'),
-    (4, 'Engineering');
+    ('IT'),
+    ('HR'),
+    ('Marketing'),
+    ('Engineering');
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+    INSERT INTO role (title, salary, department_id)
     VALUES 
-    (1, 'John', 'Smith', 1, NULL),
-    (2, 'Jane', 'Doe', 2, 1),
-    (3, 'Polly', 'Pocket', 2, 1),
-    (4, 'Jimmy', 'Tootsie', 3, 1),
-    (5, 'Mandy', 'Myers', 3, 1),
-    (6, 'Sal', 'Mirza', 3, 1),
-    (7, 'Bob', 'Bobert', 4, 1),
-    (8, 'Sally', 'Seashore', 4, 1),
-    (9, 'Joe', 'Shoeson', 4, 1);
+    ('IT Technician', 50000, 1),
+    ('HR Manager', 100000, 2),
+    ('HR Assistant', 55000, 2),
+    ('Marketing Manager', 100000, 3),
+    ('Sales Associate', 50000, 3),
+    ('Sr Sales Associate', 65000, 3),
+    ('Senior Engineer', 85000, 4),
+    ('Junior Engineer', 65000, 4),
+    ('Team Lead Engineer', 85000, 4);
 
-INSERT INTO role (id, title, salary, department_id)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES 
-    (1, 'IT Technician', 50000, 1),
-    (2, 'HR Manager', 100000, 2),
-    (3, 'HR Assistant', 55000, 2),
-    (4, 'Marketing Manager', 100000, 3),
-    (5, 'Sales Associate', 50000, 3),
-    (6, 'Sales Associate', 65000, 3),
-    (7, 'Senior Engineer', 85000, 4),
-    (8, 'Junior Engineer', 65000, 4),
-    (9, 'Senior Engineer', 85000, 4);
- 
+    ('John', 'Smith', 1, NULL),
+    ('Jane', 'Doe', 2, 1),
+    ('Polly', 'Pocket', 2, 1),
+    ('Jimmy', 'Tootsie', 3, 1),
+    ('Mandy', 'Myers', 3, 1),
+    ('Sal', 'Mirza', 3, 1),
+    ('Bob', 'Bobert', 4, 1),
+    ('Sally', 'Seashore', 4, 1),
+    ('Joe', 'Shoeson', 4, 1);
 
 
- 
